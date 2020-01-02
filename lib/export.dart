dcfbd68c7fb0e094e2493ba780011cc6a60af9c2
maliangju <mlj88mlj@126.com>
2020-01-02 16:14:24 +0800

    add sp_util

--- lib/export.dart
+++ lib/export.dart
@@ -1,3 +1,5 @@
 export 'package:fluttertoast/fluttertoast.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:device_info/device_info.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:synchronized/synchronized.dart';
